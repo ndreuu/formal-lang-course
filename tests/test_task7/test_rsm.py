0bfd906dcf81c1f499a78dfdb2ecec1d8ca704ff
Andrew <a.oleyn1kov@outlook.com>
2022-11-02 16:43:07 +0300

    task7

--- tests/test_task7/test_rsm.py
+++ tests/test_task7/test_rsm.py
@@ -4,55 +4,47 @@ from copy import deepcopy
 
 from project.ecfg import ECFG
 from project.rsm import RSM
-from tests.utils import get_data
-
-
-def _from_ecfg(rsm, ecfg):
-    acc = True
-    for var in ecfg.productions:
-        actual = rsm.boxes[var]
-        expected = ecfg.productions[var].to_epsilon_nfa()
-        acc = acc and actual.is_equivalent_to(expected)
-
-    return rsm.start == ecfg.start and len(rsm.boxes) == len(ecfg.productions) and acc
+from tests.utils import get_data, eq_automata, dot_to_nfa
 
 
 @pytest.mark.parametrize(
-    "ecfg",
+    "ecfg, expected",
     get_data(
-        "test_cfg",
+        "test_rsm",
         lambda d: (
-            ECFG.from_cfg(
-                c.CFG.from_text(d["cfg"], d["start"])
-                if d["start"] is not None
-                else c.CFG.from_text(d["cfg"])
-            )
+            ECFG.from_cfg(c.CFG.from_text(d["cfg"])),
+            RSM(
+                c.Variable(d["expected"]["start"]),
+                {
+                    c.Variable(var): dot_to_nfa(nfa)
+                    for var, nfa in d["expected"]["boxes"].items()
+                },
+            ),
         ),
     ),
 )
-def test_rsm_ecfg(ecfg: ECFG):
-    assert _from_ecfg(RSM.from_ecfg(ecfg), ecfg)
+def test_correctness_building_of_boxes(ecfg: ECFG, expected: RSM):
+    def helper(actual, expected):
+        acc = True
+        for var in expected.boxes:
+            acc = acc and eq_automata(actual.boxes[var], expected.boxes[var])
+
+        return len(actual.boxes) == len(expected.boxes) and acc
+
+    actual = RSM.from_ecfg(ecfg)
+    assert helper(actual, expected)
 
 
 @pytest.mark.parametrize(
-    "rsm",
+    "original",
     get_data(
-        "test_cfg",
-        lambda d: (
-            RSM.from_ecfg(
-                ECFG.from_cfg(
-                    c.CFG.from_text(d["cfg"], d["start"])
-                    if d["start"] is not None
-                    else c.CFG.from_text(d["cfg"])
-                )
-            )
-        ),
+        "test_rsm",
+        lambda d: RSM.from_ecfg(ECFG.from_cfg(c.CFG.from_text(d["cfg"]))),
     ),
 )
-def test_minimization(rsm: RSM):
-    actual = deepcopy(rsm).minimize()
+def test_correctness_for_minimized(original: RSM):
+    minimized = deepcopy(original).minimize()
 
-    assert len(actual.boxes) == len(rsm.boxes)
-
-    for var in rsm.boxes:
-        assert actual.boxes[var].is_equivalent_to(rsm.boxes[var])
+    assert len(minimized.boxes) == len(original.boxes)
+    for var in original.boxes:
+        assert eq_automata(minimized.boxes[var], original.boxes[var])
